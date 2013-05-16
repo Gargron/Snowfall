1db29fb99ee9c08f20dab68eb95e72e923f9f170
Eugen Rochko <eugen@zeonfederated.com>
2013-05-16 23:39:36 +0200

    Refactoring after two years

---
+++ README.md
@@ -0,0 +1,12 @@
+Snowfall.js
+===========
+
+Snowfall.js is a little thing that draws falling snow ontop of a page using a <canvas> and JavaScript.
+
+## Usage
+
+Simply include the JS in a page, and you will see the snow.
+
+## Note
+
+I have updated this little script after about 2 years of inactivity. I decided that it would be curious to see how I would have written it if I had written it now. The code quality and the performance should reflect my current state of knowledge.
--- snowfall.js
+++ snowfall.js
@@ -1,72 +1,168 @@
-(function(window, undefined) {
-    if(window.Snowfall) { return; }
+/**
+ * Copyright (c) 2013 Eugen Rochko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-    var Snowfall = function(id, number) {
-        var Field = document.getElementById(id);
-        var field = Field.getContext('2d');
-        
-            Field.width  = window.innerWidth;
-            Field.height = window.innerHeight;
+(function (window) {
+  'use strict';
 
-        var Flakes = [];
+  var Flake, Snowfall;
 
-        var Flake = function(x, y) {
-            this.x = x;
-            this.y = y;
-            this.speed_y = Math.floor(Math.random() * 3);
-            if (this.speed_y == 0) this.speed_y = 1;
+  Snowfall = function (max) {
+    if (typeof max === "undefined") {
+      max = 300;
+    }
+
+    if (!window.HTMLCanvasElement) {
+      console.warn('Snowfall.js is aborting due to the browser not supporting <canvas>');
+      return;
+    }
+
+    this.max    = max;
+    this.flakes = [];
+
+    this.createCanvas();
+    this.generateFlakes();
+    this.registerAnimation();
+    this.bindDOMEvents();
+  };
+
+  Snowfall.prototype.createCanvas = function () {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width  = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.context = this.canvas.getContext('2d');
+
+    this.canvas.setAttribute('style', 'position: absolute; top: 0; left: 0; z-index: 99999; pointer-events: none');
+    document.body.appendChild(this.canvas);
+  };
+
+  Snowfall.prototype.bindDOMEvents = function () {
+    var throttle, that;
+
+    that = this;
+
+    window.addEventListener('resize', function () {
+      if (typeof throttle === "undefined") {
+        throttle = window.setTimeout(function () {
+          throttle = undefined;
+          that.canvas.width  = window.innerWidth;
+          that.canvas.height = window.innerHeight;
+        }, 100);
+      }
+    }, false);
+  };
+
+  Snowfall.prototype.generateFlakes = function () {
+    var i;
+
+    for (i = 0; i < this.max; i += 1) {
+      this.flakes.push(new Flake(Math.floor(Math.random() * this.canvas.width), Math.floor(Math.random() * this.canvas.height * 0.45)));
+    }
+  };
+
+  Snowfall.prototype.updateFlakes = function (delta) {
+    var i, len;
+
+    for (i = 0, len = this.flakes.length; i < len; i += 1) {
+      this.flakes[i].move(delta);
+
+      if (!this.flakes[i].isVisible(this.canvas.width, this.canvas.height)) {
+        this.flakes.splice(i, 1);
+        this.flakes.push(new Flake(Math.floor(Math.random() * this.canvas.width), 0));
+      }
+    }
+  };
+
+  Snowfall.prototype.drawFrame = function () {
+    var i, len;
+
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+
+    for (i = 0, len = this.flakes.length; i < len; i += 1) {
+      this.context.fillRect(this.flakes[i].x, this.flakes[i].y, this.flakes[i].size, this.flakes[i].size);
+    }
+  };
+
+  Snowfall.prototype.registerAnimation = function () {
+    var last_run, frame, that;
+
+    if (typeof window.requestAnimationFrame === "undefined") {
+      var requestAnimationFrame = window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+      if (typeof requestAnimationFrame === "undefined") {
+        console.warn("Snowfall.js is falling back to 100ms animation intervals");
+
+        requestAnimationFrame = function (callback) {
+          return window.setTimeout(callback, 100);
         };
+      }
 
-        var generate_flakes = function(number, max_x, max_y) {
-            if (number == null) number = 300;
-            if (max_x == null)  max_x  = Field.width;
-            if (max_y == null)  max_y  = Field.height;
+      window.requestAnimationFrame = requestAnimationFrame;
+    }
 
-            for (var i = 0; i < number; i++) {
-                new_flake(max_x, max_y);
-            }
-        };
+    last_run = new Date().getTime();
+    that     = this;
 
-        var new_flake = function(max_x, max_y) {
-            Flakes.push(new Flake(Math.floor(Math.random() * max_x), Math.floor(Math.random() * max_y)));
-        };
+    frame = function (now) {
+      that.updateFlakes(now - last_run);
+      that.drawFrame();
 
-        var update_flakes = function() {
-            for (var i = 0, length = Flakes.length; i < length; i++) {
-                var flake = Flakes[i];
-                flake.y   = flake.y + flake.speed_y;
-                if (flake.x >= Field.width || flake.y >= Field.height) Flakes.splice(i, 1) && new_flake(Field.width, 10);
-            }
-        };
-
-        var draw_flakes = function() {
-            field.clearRect(0, 0, Field.width, Field.height);
-            field.fillStyle = 'rgba(255, 255, 255, 0.5)';
-
-            for (var i = 0, length = Flakes.length; i < length; i++) {
-                field.fillRect(Flakes[i].x, Flakes[i].y, 3, 3);
-            }
-        };
-
-        var animation;
-
-        this.fall = function(number) {
-            if(number == null) number = 300;
-            var count = 0;
-            generate_flakes(number);
-            animation = window.setInterval(function() {
-                update_flakes();
-                draw_flakes();
-                count++;
-            }, 70);
-        };
-
-        this.stop = function() {
-            window.clearInterval(animation);
-        };
-
-        return this;
+      last_run = now;
+      that.animation = window.requestAnimationFrame(frame);
     };
 
-    window.Snowfall = Snowfall;
-})(window);
+    this.animation = window.requestAnimationFrame(frame);
+  };
+
+  Snowfall.prototype.removeAnimation = function () {
+    if (typeof this.animation === "undefined") {
+      return;
+    }
+
+    window.cancelAnimationFrame(this.animation);
+  };
+
+  Flake = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.setSpeed();
+    this.setSize();
+  };
+
+  Flake.prototype.setSpeed = function () {
+    this.speed = Math.max(0.1, Math.random() * 0.5);
+  };
+
+  Flake.prototype.setSize = function () {
+    this.size = Math.max(1, Math.floor(Math.random() * 4));
+  };
+
+  Flake.prototype.move = function (delta) {
+    this.y += delta * this.speed;
+  };
+
+  Flake.prototype.isVisible = function (bx, by) {
+    return (this.x > 0 && this.y > 0 && this.x < bx && this.y < by);
+  };
+
+  window.Snowfall = new Snowfall(300);
+} (window));
